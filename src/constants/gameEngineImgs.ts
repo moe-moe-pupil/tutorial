dafb0f2008e2ca90e5443ad92d5b1dd509ffda4b
MoeThun <dhtmitmi@gmail.com>
2023-11-20 18:49:07 +0800

    chore: add many imgs

--- src/constants/gameEngineImgs.ts
+++ src/constants/gameEngineImgs.ts
@@ -1,5 +1,10 @@
 import defold from '../assets/image/defold.png'
 import unreal from '../assets/image/ue.png'
 import unity from '../assets/image/unity.png'
+import gamemaker from '../assets/image/gamemaker.png'
+import phaser from '../assets/image/phaser.jpg'
+import cocos from '../assets/image/cocos.png'
+import godot from '../assets/image/godot.png'
+import firebear from '../assets/image/firebear.png'
 
-export const GAMEENGINEIMGS = [defold, unreal, unity]
+export const GAMEENGINEIMGS = [defold, unreal, unity, gamemaker, phaser, cocos, godot, firebear]
